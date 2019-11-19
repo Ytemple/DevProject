5eb8be76b5f1909708a1bf66f6bc745375eae432
Ytemple <1911311017@qq.com>
2019-11-19 16:18:50 +0800

    enter

--- src/index.jsx
+++ src/index.jsx
@@ -7,5 +7,5 @@ const ICE_CONTAINER = document.getElementById('ice-container');
 if (!ICE_CONTAINER) {
   throw new Error('当前页面不存在 <div id="ice-container"></div> 节点.');
 }
-
+//233
 ReactDOM.render(router(), ICE_CONTAINER);
