d5eb8e3ecdbf5a1c869bfd6ee92c6b1a9d6d7336
Ytemple <1911311017@qq.com>
2019-11-20 11:48:12 +0800

    11:48

--- src/Common.js
+++ src/Common.js
@@ -1,6 +1,6 @@
 const headerToken ='Bearer eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyTmFtZSI6InJvb3QiLCJleHAiOjE1NjkxNzM4OTR9.AajK9v0wzR3w4uu85rtHkAgvhmgEVfZVFiu2pGN7odkgLb4uQtxYbuaWiJXZmJ4pO3bxACUjFOA0i92RBFqcQw'
 const hostPort ='http://localhost:9001/'
  //233
- //244
+ 
 export {headerToken,hostPort}
 //  shuju.flag == true
