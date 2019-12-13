dd222e7ed89e8793d8aa0abe5ca6f7fb536171fd
Ytemple <1911311017@qq.com>
2019-12-13 15:19:09 +0800

    style

--- src/pages/DataStorage/Image/Image.jsx
+++ src/pages/DataStorage/Image/Image.jsx
@@ -10,8 +10,7 @@ import Table2 from './Table2/index'
 import store from '../../Store/index'
 import PropTypes from 'prop-types';
 import {headerToken,hostPort} from '../../../Common'
-import styles from './Image.scss';
-
+import styles from './index.module.scss';
 
 let data1 = [];
 let n1 = 1111;
@@ -292,15 +291,15 @@ export default class Image extends Component {
 
   render() {
     return (
-      <div className="home-page" style={styles.homePage}>
+      <div className={styles.home} >
+      <div  className={styles.tree} >
         <TreeIndex
-        className={styles.tree}
           dataConfig={this.state.dataConfig}
           onSelectBlock={this.onSelectBlock}
           getChildrenData={this.getChildrenData}
           getTreekey={this.getTreekey}  //一个是获取树节点，还有就是通过获取的这个树节点去获取文档列表
         ></TreeIndex>
-       
+       </div>
         <div style={{ display: this.state.displayName }}>
           <Table1
             childrenData={this.state.childrenData}
@@ -318,11 +317,11 @@ export default class Image extends Component {
     );
   }
 }
-
+/** 
 const styles = {
   homePage: {   //在这儿就设置了弹性，所以才有了后来项目的弹性
-    display: 'flex',
-    flexDirection: 'row',
+    
+   
     //height: '100vh',
   },
   mainContent: {   //这个我没有找到，这个是用来设置弹性项目的
@@ -335,6 +334,11 @@ const styles = {
   },
   table: {
     display: 'block',
-  }
+  },
+  tree: {
+    marginLeft: '1200px',
+    color:'red'
+  },
 };
 
+*/
